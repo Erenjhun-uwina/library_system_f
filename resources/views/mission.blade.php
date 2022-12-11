10d03044ad723630b165b350f2c0d05ce06c84f3
jeyel <padauan.jerecalyn.cuenca@gmail.com>
2022-12-11 16:26:39 +0800

    mvd

---
+++ resources/views/mission.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.main')
+
+
+@section('title')
+    mission 
+@stop
+
+
+@section('body')
+
+<h2> Mission </h2><br>
+<p> To give college students the knowledge they need to learn better and assist them in
+    developing the research skills needed for a blended learning environment and to assists
+    students and admin staff. </p>
+
+@stop
