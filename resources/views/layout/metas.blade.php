74b3901a1c8be2b9e667cd853b807f081068313e
enerjhun relon <enerjhunrelon@gmail.com>
2022-12-11 14:06:22 +0800

    css, structures, layouts added

---
+++ resources/views/layout/metas.blade.php
@@ -0,0 +1,19 @@
+{{-- wag nato galawin pabura nalang pag ipapasa nio na --}}
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    {{-- global css kung meron man --}}
+   
+    <link rel="stylesheet" href="/css/app.css">
+    <title>@yield('title')</title>
+
+    @yield('head')
+</head>
+<body>
+    @yield('body')
+</body>
+</html>
