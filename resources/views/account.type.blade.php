6f875e3a54fc94df73186457ac208a47c087ddaf
enerjhun relon <enerjhunrelon@gmail.com>
2022-12-11 15:44:17 +0800

    gamitin nio nman ung bladeee syntaxxxxx sayangggg

--- resources/views/account.type.blade.php
+++ resources/views/account.type.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -21,4 +21,34 @@
     </center>
 
 </body>
-</html>
+</html> --}}
+
+
+@extends('layout.metas')
+
+@section('title', 'account type select')
+
+@section()
+    <center>
+        <img id="logo" src="https://www.iconpacks.net/icons/2/free-opened-book-icon-3163-thumb.png" alt="">
+
+        <h1>QCU<br>Library System </h1>
+        <p>"When in doubt go to the library."</p>
+        <p>J.K. Rowling </p>
+
+        <div id="btn_con">
+            <a id="Borrower" href="login/borrower">Borrower</a><br>
+            <a id="librarian_btn" href="login/admin">ADMIN</a>
+        </div>
+    </center>
+@endsection
+
+
+
+
+
+
+
+
+
+
