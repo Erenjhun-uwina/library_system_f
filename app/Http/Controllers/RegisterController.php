2c221d100ad9189b7d880f6ebb04f1219b46e33a
enerjhun relon <enerjhunrelon@gmail.com>
2022-12-12 08:56:31 +0800

    not final

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -18,7 +18,11 @@ class RegisterController extends Controller
         'confirm_pass' => 'required'
     ];
     private  $admin_rules = ['uname' => 'required'];
-    private $borrower_rules = ['contact_no'=>'required','email'=>"required"];
+    private $borrower_rules = [
+        'contact_no' => 'required',
+        'email' => "required",
+        "id_no"=>'regex:/([0-9]{2})-([0-9]{4,5})/u',
+    ];
 
 
     public function register_page(Request $req)
@@ -33,14 +37,25 @@ class RegisterController extends Controller
     {
         $acc_type = $req->acc_type;
 
-        $rules = 
+        $rules = array_merge([$this->rules, ($acc_type == 'admin') ? $this->admin_rules : $this->borrower_rules]);
 
         $validator = Validator::make($req->all(), $this->rules);
+
+        if ($validator->fails()) {
+            return redirect('register/' . $acc_type)->withErrors($validator);
+        }
+
         $inputs = $validator->valid();
+
+
         $model = $modelService->get_acc_model($acc_type);
         $acc = $modelService->exist($model, 'uname', $inputs['uname']);
 
         if ($acc) return redirect('register/' . $acc_type)->withErrors('this user is already regitered');
         return back();
+    }
+
+    private function check_admin()
+    {
     }
 }
