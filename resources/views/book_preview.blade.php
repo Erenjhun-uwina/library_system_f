d29f06bdba616745fcdc6ce4926c5e22a86d5741
enerjhun relon <enerjhunrelon@gmail.com>
2022-12-14 11:53:13 +0800

    redesign

--- resources/views/book_preview.blade.php
+++ resources/views/book_preview.blade.php
@@ -5,7 +5,7 @@
 @section('head')
     @parent
     <link rel="stylesheet" href="/css/bookshelf.css">
-    
+    <link rel="stylesheet" href="/css/preview.css">
 @endsection
 
 
@@ -14,13 +14,23 @@
     @parent
 
     <div class='shelves'>
-        <img src="/assets/covers/{{$book->cover}}" alt="">
-        <p>{{$book->title}}</p>
-        <p>Author:{{$book->author}}</p>
-        <P>release date:{{$book->date_release}}</p>
-        <p>categoty:{{$book->category}}</p>
+        <img src="/assets/covers/{{ $book->cover }}" class ='cover'>
 
-        <form action="/request" method="post"></form>
+        <section class='details'>
+
+            <p>{{ $book->title }}</p>
+            <p>Author:{{ $book->author }}</p>
+            <P>release date:{{ $book->date_release }}</p>
+            <p>categoty:{{ $book->category }}</p>
+
+             @if (session('acc_type') != 'admin')
+            <form action="/borrow" method="post">
+                @csrf
+                <input type="hidden" name="book_id" value="{{ $book->id }}">
+                <button type="submit">borrow</button>
+            </form>
+        @endif
+        </section>
     </div>
 
 @endsection
